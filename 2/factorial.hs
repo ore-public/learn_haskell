a5c83e4ba59c67dbfc644563a569061f7cfe81f0
m-konishi <konishi@office-ore.net>
2012-06-01 12:35:37 +0900

    Doubleの型の例。Floatの型の関数より、実行結果の精度が上がってるのが分かる

--- 2/factorial.hs
+++ 2/factorial.hs
@@ -1,6 +1,9 @@
-factorial :: Integer -> Intjger
+factorial :: Integer -> Integer
 factorial n = product [1..n]
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
 
