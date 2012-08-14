5f006d8cb89800d6b06eedb4bf4a7e907ebf2155
m-konishi <konishi@office-ore.net>
2012-08-14 12:16:52 +0900

    greetの改良。
    違うパターンの関数本体で共有されないので、whereではなくグローバルで定義しないと駄目。
    （この部分、意味がよく分からない。【P.44 whereのスコープ】)

---
+++ 3/greet_kai.hs
@@ -0,0 +1,10 @@
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juan!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
