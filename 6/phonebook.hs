5a08c542560cfc24e7e6747315205fa9b7e93a81
Masaya Konishi <konishi@office-ore.net>
2013-01-01 15:03:18 +0900

    Data.Map Mapを使って色々

---
+++ 6/phonebook.hs
@@ -0,0 +1,28 @@
+import qualified Data.Map as Map
+import Data.Char
+
+phonebook :: Map.Map String String
+phonebook = Map.fromList $
+            [("betty", "555-1234")
+            ,("hoge", "123-4567")
+            ]
+string2digit :: String -> [Int]
+string2digit = map digitToInt . filter isDigit
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith add xs
+    where add number1 number2 = number1 ++ ", " ++ number2
+
+phoneBook :: Map.Map String String
+phoneBook = phoneBookToMap $
+            [("betty", "555-2938")
+            ,("betty", "342-2492")
+            ,("bonnie", "452-2928")
+            ,("patsy", "493-2928")
+            ,("patsy", "943-2929")
+            ,("patsy", "827-9162")
+            ,("lucille", "205-2928")
+            ,("wendy", "939-8282")
+            ,("penny", "853-2492")
+            ,("penny", "555-2111")
+            ]
